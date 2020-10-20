99843ebc2c3d16965159756c14179803021ed6d4
Matteo Ferrarese <matteo.ferrarese@yahoo.it>
2020-10-20 21:47:27 +0200

    Html + Js concluso

--- js/myscript.js
+++ js/myscript.js
@@ -1,9 +1,11 @@
+var elementoOutput = document.getElementById("titolo");
+
 // richiesta numero chilometri
-var richiestaKm = prompt("quanti km");
+var richiestaKm = prompt("Quanti km?");
 console.log("quanti km: ", richiestaKm);
 
 // richiesta età passeggero
-var richiestaEta = prompt("età");
+var richiestaEta = prompt("Età?");
 console.log("età: ", richiestaEta);
 
 // prezzo biglietto
@@ -11,21 +13,21 @@ var price = 0.21;
 console.log("prezzo: ", 0.21 * richiestaKm);
 
 // sconto minorenne
-var scontoMinorenne = 20 * richiestaKm / 100;
+var scontoMinorenne = 80 * (richiestaKm * 0.21) / 100;
 
 // sconto over65
-var scontoOver65 = 40 * richiestaKm / 100;
+var scontoOver65 = 60 * (richiestaKm * 0.21) / 100;
 
 // condizioni età
 if (richiestaEta <= 17) {
   // sconto 20%
+  elementoOutput.innerHTML = "Minorenne! Riceverai uno sconto di: " + scontoMinorenne.toFixed(2) + "€";
   console.log("minorenne!", scontoMinorenne);
 } else if (richiestaEta >= 66) {
   // sconto 40%
+  elementoOutput.innerHTML = "Over 65! Riceverai uno sconto di: " + scontoOver65.toFixed(2) + "€";
   console.log("over 65!", scontoOver65);
 } else {
+  elementoOutput.innerHTML = "Nessuno sconto!"
   console.log("nessuno sconto");
 }
-
-
-// prezzo totale del viaggio
